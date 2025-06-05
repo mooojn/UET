7fb3c7060b014cb41078de5a4c2be9fd554fb697
Munees Tariq <144435962+mooojn@users.noreply.github.com>
2025-06-05 17:08:14 +0500

    Delete Study-Material/Semester-3 directory

--- Study-Material/Semester-3/Data Structures and Algorithms LAB/Lab02/save_in_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def sorted_array(file_name, sorted_array):
-    with open(file_name, 'w', newline='\n') as f:
-        writer = csv.writer(f)
-        for el in sorted_array:
-            writer.writerow([el])
